e333962de96847ac41eab867019ba147ece98492
MRAlexandre-Web <mitchalexand@gmail.com>
2023-08-06 19:40:33 -0400

    Navbar Created

---
+++ Intro Page/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "../index.css";
+import logo from "../assets/logo.svg";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className="bg-white p-4">
+        <div className="flex justify-between">
+          <img src={logo} alt="Snap logo" />
+          <svg width="32" height="18" xmlns="http://www.w3.org/2000/svg">
+            <g fill="#151515" fillRule="evenodd">
+              <path d="M0 0h32v2H0zM0 8h32v2H0zM0 16h32v2H0z" />
+            </g>
+          </svg>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
